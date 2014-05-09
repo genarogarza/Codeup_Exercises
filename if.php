4102815711c0dfffb3e2f0f95a184ed5693969c3
Genaro Garza <genarogarza@MyMacbook.local>
2014-05-09 14:32:35 -0500

    hope this works

--- if.php
+++ if.php
@@ -4,44 +4,59 @@ $a = 5;
 $b = 10;
 $c = '10';
 
-if ($a < $b) {
+// Add an ELSE clause to the next 2 statements into an if/else
+if ($a < $b) 
+{
     // output the appropriate result
-    echo "line 9 $a is less than $b\n";
+    echo "line " .__LINE__ . "> $a is less than $b\n";
+}
+ELSE 
+
+    // output the appropriate result
+if ($b >$a)
+{
+    echo "line " .__LINE__ . "> $b is greater than $a\n";
 }
 
-if ($b > $a) {
-    // output the appropriate result
-    echo "line 14 $b is greater than $a\n";
-}
 
-if ($b >= $c) {
+//-------------------------------------------------
+// Shorten the next 2 statements into an if/else
+if ($b >= $c) 
+{
     // output the appropriate result
-    echo "line 19 $b is greater than or equal to $c\n";
+    echo "line " .__LINE__ . "> $b is >= $c\n";
 }
 
 if ($b <= $c) {
+
     // output the appropriate result
-    echo "line 24 $b is less than or equal to $c\n";
+    echo "line " .__LINE__ . "> $b is <= $c\n";
 }
 
+
+//-----------------------------------------------
+
+
+
+
+// combine the next 4 conditionals into 
+// one if/else/elseif block that checks in order for:
+// identical, equal, not identical, not equal
 if ($b == $c) {
     // output the appropriate result
-    echo "line 29 $b is same value as $c\n";
-}
-
-if ($b === $c) {
+    echo "line " .__LINE__ . "> $b is equal to $c\n";
+} 
+elseif ($b === $c) {
     // output the appropriate result
-    echo " line 34 $b is same value and type as $c\n";
+    echo "line " .__LINE__ . "> $b is identical to $c\n";
 }
 
-// output the appropriate result
-if ($b != $c) {
-    echo "line 39 $b is not equal to $c\n";
-}
+elseif ($b != $c) {
+    // output the appropriate result
+    echo "line " .__LINE__ . "> $b is not equal to $c\n";
+} 
 
-// output the appropriate result
-if ($b !== $c) {
-    echo "line 44 $b is not identical to $c\n";
+elseif ($b !== $c) {
+    // output the appropriate result
+    echo "line " .__LINE__ . "> $b is not identical to $c\n";
 }
-
-?>
