d3818047ddebe89e4cf9ad6b19b02a31cbe7bf1a
Genaro Garza <genarogarza@MyMacbook.local>
2014-05-15 12:41:21 -0500

    version 1

--- arithmetic.php
+++ arithmetic.php
@@ -1,32 +1,96 @@
 <?php
 
+
+function test($a, $b) {
+	if (!is_numeric($a) || !is_numeric($b)) {
+		 echo ' ERROR: both $a ans $b should be numbers' . PHP_EOL;
+		 exit(1);
+	} else echo '';
+}
+
 function add($a, $b) {
-    echo $a + $b . PHP_EOL;
+	test($a, $b);
+	
+	echo $a + $b . PHP_EOL;
 }
 
 function subtract($a, $b) {
-    echo $a - $b . PHP_EOL;
+	test($a, $b);
+	echo $a - $b . PHP_EOL;
 }
 
 function multiply($a, $b) {
-    echo $a * $b . PHP_EOL;
+	test($a, $b);
+	echo $a * $b . PHP_EOL;
 }
 
 function divide($a, $b) {
-    echo $a / $b . PHP_EOL;
+	test($a, $b);
+	if ($b !== 0) {
+		echo $a / $b . PHP_EOL;
+}		else echo 'ERROR : division by zero' . PHP_EOL;
+		
 }
 
 function modulus($a, $b) {
-	echo $a % $b  . PHP_EOL;
+	test($a, $b);
+	if ($b !== 0) {
+		echo $a / $b . PHP_EOL;
+}		else echo 'ERROR : division by zero' . PHP_EOL;
 }
 
-add(20,20);       //outputs 40
+
+add(29,20);       //outputs 40
 subtract(20,20);  //outputs 0
 multiply(20,20);  //outputs 400
-divide(20,20);    //outputs 1
-modulus(5,3)	  //outputs 2
+divide(20,0);    //outputs 1
+modulus(5,0)	  //outputs 2
 
 
 
+//--------------------------------------------
+// function compare($a, $b, $strict = true)      //defaults $strict to true
+// {
+// 	if ($strict === true){
+// 		echo $a === $b ? 'true' : 'false';
 
+// 	} else {
+// 		echo $a == $b ?	'true' : 'false';
+
+// 	}
+// 	echo PHP_EOL;
+// }
+
+// compare(false, false);  //true
+// compare(42, '42')       //false
+
+//------------------------------------------
+
+// function inspect($variable = null; $dump = true)
+// {
+// 	if ($dump === true) {
+// 		var_dump($variable)
+// 	}else {
+// 		print_r($variable);
+// 	}
+
+// }
+
+// inspect(['batman', 'superman', 'spiderman']);
+
+// //------										//  these two are same result
+
+// $heroes = ['batman', 'superman', 'spiderman']
+// inspect($heroes)
+
+// //-------------------------------------------
+
+// function adder($a, $b) {
+// 	if is_numeric($a) && is_numeric($b)
+// 		echo $a + $b;
+// }	else {
+// 	echo ' both $a ans $b should be numbers';
+
+// }
+// 	echo PHP_EOL;
 ?>
