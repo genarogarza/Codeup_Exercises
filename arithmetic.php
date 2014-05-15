29aa4bb5ddd782020648b0039616244241d1b814
Genaro Garza <genarogarza@MyMacbook.local>
2014-05-15 15:31:09 -0500

    version 1

--- arithmetic.php
+++ arithmetic.php
@@ -1,16 +1,25 @@
 <?php
 
+// function test($a, $b) {
+// 	if (!is_numeric($a) || !is_numeric($b)) {
+// 		echo 'ERROR: both $a ans $b should be numbers' . PHP_EOL;
+// 		 //exit(1);  use if $a and $b are actual user assigned variables
+// 	};
+
 
 function test($a, $b) {
-	if (!is_numeric($a) || !is_numeric($b)) {
-		 echo ' ERROR: both $a ans $b should be numbers' . PHP_EOL;
-		 exit(1);
-	} else echo '';
+	if (!is_numeric($a)) {
+		echo "ERROR: {$a} should be a number" . PHP_EOL;
+		 
+	} elseif (!is_numeric($b)) {
+		echo "ERROR: {$b} should be a number" . PHP_EOL;
+	}	
+	
 }
+
 
 function add($a, $b) {
 	test($a, $b);
-	
 	echo $a + $b . PHP_EOL;
 }
 
@@ -28,23 +37,22 @@ function divide($a, $b) {
 	test($a, $b);
 	if ($b !== 0) {
 		echo $a / $b . PHP_EOL;
-}		else echo 'ERROR : division by zero' . PHP_EOL;
-		
+	}		else echo 'ERROR : division by zero' . PHP_EOL;
 }
 
 function modulus($a, $b) {
 	test($a, $b);
 	if ($b !== 0) {
 		echo $a / $b . PHP_EOL;
-}		else echo 'ERROR : division by zero' . PHP_EOL;
+	}		else echo 'ERROR : division by zero' . PHP_EOL;
 }
 
 
-add(29,20);       //outputs 40
+add("a20",20);       //outputs 40
 subtract(20,20);  //outputs 0
 multiply(20,20);  //outputs 400
-divide(20,0);    //outputs 1
-modulus(5,0)	  //outputs 2
+divide(20,20);    //outputs 1
+modulus(20,20)	  //outputs 1
 
 
 
